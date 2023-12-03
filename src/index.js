974188db83c6db69ef4408daf8e01191e5703cdf
VladyslavTodo <124445034+VladyslavTodo@users.noreply.github.com>
2023-12-03 14:55:34 +0200

    done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { Global, ThemeProvider } from '@emotion/react';
 import 'modern-normalize';
 
-import { App } from 'components';
+// import { App } from 'components';
+import  App  from './components/App/App';
 import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
